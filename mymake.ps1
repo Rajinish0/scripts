21c5321ab8271ecfd49ceda1fff1d4b948dc588f
Rajinish0 <66392735+Rajinish0@users.noreply.github.com>
2022-12-21 23:14:16 +0100

    Add files via upload

---
+++ mymake.ps1
@@ -0,0 +1,23 @@
+﻿param(
+[parameter(Mandatory, HelpMessage="Enter an output file : ")]
+$filename
+)
+
+if ( -not (Test-Path ".\log.txt") ){
+    ni .\log.txt
+    $lastDate = Get-Date -Year 1 -Month 1 -Day 1
+}
+
+else {
+    $h = cat ".\log.txt"
+    $lastDate = [DateTime]::Parse($h)
+}
+
+Get-Date > .\log.txt
+
+#recompiling
+gci "." | where {( ($_.Extension -eq ".cpp") -and ($_.LastWriteTime -gt $lastDate) )} | % {g++ -c $_}
+
+#linking 
+g++ *.o glad.c -lglfw3 -lopengl32 -lgdi32 -o $filename
+
