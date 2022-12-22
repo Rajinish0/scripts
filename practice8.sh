59e9114f9c2e52d30b108295f7c85f2a52909c01
Rajinish0 <66392735+Rajinish0@users.noreply.github.com>
2022-12-23 00:40:14 +0100

    Add files via upload

---
+++ practice8.sh
@@ -0,0 +1,15 @@
+## tried extrapolating my knowledge of recursion to scripts
+## if a function can call itself, so can a script probably.
+## and this works alright
+#im not checking for which one is greater because they get swapped if the second one is greater than the first one in 
+## the recursion step
+
+
+if [ $(($1 % $2)) = 0 ]
+then
+	echo $2
+else 
+	bash practice8.sh $2 $(($1 % $2))
+fi
+
+
