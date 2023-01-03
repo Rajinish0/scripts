dcb67c0c0026fc68e24208ce3289838d0cd3bd1d
Rajinish0 <66392735+Rajinish0@users.noreply.github.com>
2023-01-03 04:14:30 +0100

    Update mymake.ps1

--- mymake.ps1
+++ mymake.ps1
@@ -1,5 +1,6 @@
 # couldn't figure out how to use cmake, so this script just recompiles the newly modified c++ files for my opengl project 
-# and then links all the object files together to create an executable file. Saves the time of recompling everything when i use g++ *.h *cpp
+# and then links all the object files together to create an executable file. Saves the time of recompling everything if i use g++ *.cpp
+# now it can detect those cpp files which contain headers that were changed, those need to be recompiled too.
 
 param(
 [parameter(Mandatory, HelpMessage="Enter an output file : ")]
@@ -17,15 +18,20 @@ else {
 }
 
 Get-Date > .\log.txt
+echo $lastDate
 
 $arr = @()
 
+echo "getting modified header files.."
+
 gci "." | where {( ($_.Extension -eq ".h") -and ($_.LastWriteTime -gt $lastDate) )} | % {$arr += $_.Name}
+$arr
+echo "done"
 
 function myfunc(){
-param([String] $s) 
+param($s) 
 foreach ($i in $arr){
-    if ($s.Contains($i)){
+    if ($s.Contains("#include `"$i`"")){
     return $true
     }
 }
@@ -33,11 +39,17 @@ return $false
 }
 
 
-#recompiling
-gci "." | where {( ($_.Extension -eq ".cpp") -and ($_.LastWriteTime -gt $lastDate) ) -or (myfunc([string] $(cat $_.Name))) } | % {g++ -c $_}
 
+Write-Host "compiling altered files.."
+#recompiling
+gci "." | where { ($_.Extension -eq ".cpp") -and ( ($_.LastWriteTime -gt $lastDate) -or ( $(myfunc(cat $_.Name)) ) ) } | % {g++ -c $_}
+Write-Host "done"
+
+Write-Host "linking"
 #linking 
 g++ *.o glad.c -lglfw3 -lopengl32 -lgdi32 -o $filename
+
+
 
 
 
